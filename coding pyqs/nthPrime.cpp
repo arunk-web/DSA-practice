42b7014891753cdd8100d5469e3acea47e4ec41e
arunk-web <arunk02615@gmail.com>
2026-08-06 23:49:55 +0530

    added pyqs

--- coding pyqs/nthPrime.cpp
+++ coding pyqs/nthPrime.cpp
@@ -4,8 +4,8 @@ using namespace std;
 bool isPrime(int n){
     if(n <= 1) return false;
 
-    for(int i = 2 ; i*i <= n ; i++){
-        if(n%i == 0){
+    for(int i = 2 ;  i*i <= n ; i++){
+        if(n%i == 0   ){
             return false;
         }
     }
@@ -16,16 +16,16 @@ int findnthPrime(int n){
     int cnt = 0;
 
     for(int i = 1 ; ; i++){
-        if(isPrime(i)){
+        if(isPrime(i) ){
             cnt++;
 
-            if(cnt == n) return i;
+            if(cnt == n)  return i;
         }
     }
-    return -1;
+    return  -1;
 }
 int main(){
-    int n1,n2; cin>>n1>>n2;
+    int n1,n2;  cin>>n1>>n2;
 
     int first = findnthPrime(n1);
     int second = findnthPrime(n2);
